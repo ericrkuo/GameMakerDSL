7a28893d3cefdbb5e27d60865099ad069a05e9e7
jinxuan2000 <jinxuan2000@gmail.com>
2022-10-05 21:51:03 -0700

    added fireball and portal to ui

--- src/ast/fireball/Fireball.java
+++ src/ast/fireball/Fireball.java
@@ -2,12 +2,25 @@ package ast.fireball;
 
 import ast.Obstacle;
 import ui.CollisionVisitor;
+import ui.Util;
+
+import java.awt.*;
 
 public abstract class Fireball extends Obstacle {
-    int initY;
-    int speed;
+    int fireballSpeed;
+    public static final Image image = Util.loadImage("assets/fireball.png"); // need t
     @Override
     public <C, T> T accept(C context, CollisionVisitor<C, T> v) {
         return null;
     }
+
+    public int getFireballSpeed() {
+        return fireballSpeed;
+    }
+
+    public void setSpeed(int speed) {
+        this.fireballSpeed = speed;
+    }
+
+
 }
